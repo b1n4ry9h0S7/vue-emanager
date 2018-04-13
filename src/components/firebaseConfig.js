e3afc85ba6e01186ade7509acf1230780fd10bb6
nishanthdev <nishathbht@gmail.com>
2018-04-13 21:03:38 +0530

    First Commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyDiHQ4hUylmJwxq6exdL9TNFlkMfheddKQ",
+    authDomain: "vue-dev-fd69a.firebaseapp.com",
+    databaseURL: "https://vue-dev-fd69a.firebaseio.com",
+    projectId: "vue-dev-fd69a",
+    storageBucket: "vue-dev-fd69a.appspot.com",
+    messagingSenderId: "994665124348"
+} 
